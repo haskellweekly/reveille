0d73aaa07335ca2cf317b752c1619c126155987d
Taylor Fausak <taylor@fausak.me>
2018-02-13 09:04:08 -0500

    Run the updater once a minute

--- aggie.hs
+++ aggie.hs
@@ -2,9 +2,11 @@ module Main
   ( main
   ) where
 
+import qualified Control.Concurrent as Concurrent
 import qualified Control.Concurrent.Async as Async
 import qualified Control.Concurrent.STM as Stm
 import qualified Control.Exception as Exception
+import qualified Control.Monad as Monad
 import qualified Data.Foldable as Foldable
 import qualified Data.List as List
 import qualified Data.Map as Map
@@ -34,22 +36,29 @@ main = do
     (startUpdater manager database)
     (startServer database)
 
-startUpdater manager database = Foldable.for_ authors (\ author -> do
-  Printf.printf "- %s <%s>\n"
-    (fromName (authorName author))
-    (fromUrl (authorUrl author))
+startUpdater :: Client.Manager -> Stm.TVar Database -> IO ()
+startUpdater manager database = Monad.forever (do
+  Foldable.for_ authors (\ author -> do
+    Printf.printf "- %s <%s>\n"
+      (fromName (authorName author))
+      (fromUrl (authorUrl author))
 
-  items <- Exception.catch
-    (getAuthorItems manager author)
-    (\ exception -> do
-      print (exception :: Exception.IOException)
-      pure Set.empty)
-  Stm.atomically (Stm.modifyTVar database (updateDatabase author items))
+    items <- Exception.catch
+      (getAuthorItems manager author)
+      (\ exception -> do
+        print (exception :: Exception.IOException)
+        pure Set.empty)
+    Stm.atomically (Stm.modifyTVar database (updateDatabase author items))
 
-  Foldable.for_ items (\ item -> do
-    Printf.printf "  - %s: %s\n"
-      (rfc3339 (Maybe.fromMaybe unixEpoch (itemTime item)))
-      (fromName (itemName item))))
+    Foldable.for_ items (\ item -> do
+      Printf.printf "  - %s: %s\n"
+        (rfc3339 (Maybe.fromMaybe unixEpoch (itemTime item)))
+        (fromName (itemName item))))
+
+  sleep 60)
+
+sleep :: Int -> IO ()
+sleep seconds = Concurrent.threadDelay (seconds * 1000000)
 
 startServer :: Stm.TVar Database -> IO ()
 startServer database = Warp.run 3000 (\ request respond -> do
