17ab8d21a40d4cdf306acfcc0f61c02ac163f99f
Taylor Fausak <taylor@fausak.me>
2018-02-15 17:51:45 -0500

    Reject empty names

--- library/Reveille/Name.hs
+++ library/Reveille/Name.hs
@@ -4,14 +4,17 @@ module Reveille.Name
   , fromName
   ) where
 
+import qualified Control.Monad as Monad
 import qualified Data.Text as Text
 
 newtype Name = Name
   { unwrapName :: Text.Text
   } deriving (Eq, Ord, Show)
 
-toName :: String -> Either () Name
-toName string = Right (Name (Text.pack string))
+toName :: String -> Either String Name
+toName string = do
+  Monad.when (null string) (fail "empty name")
+  pure (Name (Text.pack string))
 
 fromName :: Name -> String
 fromName name = Text.unpack (unwrapName name)
