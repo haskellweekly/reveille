27613a1085401648e078dc40b7e3e46ded2b7840
Taylor Fausak <taylor@fausak.me>
2018-02-16 09:58:58 -0500

    Split up the server

--- library/Reveille.hs
+++ library/Reveille.hs
@@ -71,110 +71,131 @@ sleep :: Int -> IO ()
 sleep seconds = Concurrent.threadDelay (seconds * 1000000)
 
 startServer :: Stm.TVar Database -> IO ()
-startServer database = Warp.run 3000 (\ request respond -> do
+startServer database = Warp.run 3000 (makeApplication database)
+
+makeApplication :: Stm.TVar Database -> Wai.Application
+makeApplication database request respond = do
   let method = Text.unpack (Text.decodeUtf8 (Wai.requestMethod request))
   let path = map Text.unpack (Wai.pathInfo request)
-  case (method, path) of
-    ("GET", ["feed.atom"]) -> do
-      db <- Stm.readTVarIO database
-      now <- Time.getCurrentTime
-      let items = getRecentDatabaseItems db now
-      let entries = map itemToEntry items
-      let feed = xmlElement
-            "feed"
-            [("xmlns", "http://www.w3.org/2005/Atom")]
-            ( xmlNode "title" [] [xmlContent "Haskell Weekly"]
-            : xmlNode "id" [] [xmlContent "https://haskellweekly.news/"]
-            : xmlNode "updated" [] [xmlContent (rfc3339 now)]
-            : xmlNode "link" [("rel", "self"), ("href", "http://localhost:3000/feed.atom")] []
-            : entries
-            )
-      let document = xmlDocument feed
-      respond (Wai.responseLBS
-        Http.ok200
-        [(Http.hContentType, Text.encodeUtf8 (Text.pack "application/atom+xml"))]
-        (Xml.renderLBS Xml.def document))
-    ("GET", ["health-check"]) -> respond
-      (Wai.responseLBS Http.ok200 [] LazyBytes.empty)
-    ("GET", ["robots.txt"]) -> respond (Wai.responseLBS
-      Http.ok200
-      []
-      (LazyBytes.fromStrict (Text.encodeUtf8 (Text.pack (unlines
-        [ "User-Agent: *"
-        , "Disallow:"
-        ])))))
-    ("GET", ["favicon.ico"]) -> respond (Wai.responseLBS
-      Http.ok200
-      []
-      (LazyBytes.pack
-        [ 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x10, 0x10
-        , 0x02, 0x00, 0x01, 0x00, 0x01, 0x00, 0xb0, 0x00
-        , 0x00, 0x00, 0x16, 0x00, 0x00, 0x00, 0x28, 0x00
-        , 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x20, 0x00
-        , 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00
-        , 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x79, 0x48
-        , 0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-        ]))
-    ("GET", []) -> do
-      db <- Stm.readTVarIO database
-      now <- Time.getCurrentTime
-      let items = getRecentDatabaseItems db now
-      let
-        settings = Xml.def { Xml.rsXMLDeclaration = False }
-        doctype = Xml.Doctype (Text.pack "html") Nothing
-        prologue = Xml.Prologue [] (Just doctype) []
-        htmlHead = xmlNode "head" []
-          [ xmlNode "meta" [("charset", "utf-8")] []
-          , xmlNode "title" [] [xmlContent "Haskell Weekly"]
-          , xmlNode "link"
-            [ ("rel", "alternate")
-            , ("type", "application/atom+xml")
-            , ("href", "feed.atom")
-            ] []
-          ]
-        htmlBody = xmlNode "body" []
-          [ xmlNode "h1" [] [xmlContent "Haskell Weekly"]
-          , xmlNode "p" []
-            [ xmlContent "Subscribe to "
-            , xmlNode "a" [("href", "feed.atom")] [xmlContent "the Atom feed"]
-            , xmlContent "."
-            ]
-          , xmlNode "ol" [] (map
-            (\ (author, item) -> xmlNode "li" []
-              [ xmlNode "a" [("href", fromUrl (itemUrl item))] [xmlContent (fromName (itemName item))]
-              , xmlContent " by "
-              , xmlContent (fromName (authorName author))
-              , xmlContent " on "
-              , xmlContent (Time.formatTime Time.defaultTimeLocale "%B %-e" (itemTime item))
-              ])
-            items)
-          ]
-        html = xmlElement "html" [] [htmlHead, htmlBody]
-        document = Xml.Document prologue html []
-        body = Xml.renderLBS settings document
-      respond (Wai.responseLBS
-        Http.ok200
-        [(Http.hContentType, Text.encodeUtf8 (Text.pack "text/html; charset=utf-8"))]
-        body)
-    _ -> respond (Wai.responseLBS Http.notFound404 [] mempty))
+  response <- case (method, path) of
+    ("GET", ["feed.atom"]) -> getFeedHandler database
+    ("GET", ["health-check"]) -> getHealthCheckHandler
+    ("GET", ["robots.txt"]) -> getRobotsHandler
+    ("GET", ["favicon.ico"]) -> getFaviconHandler
+    ("GET", []) -> getIndexHandler database
+    _ -> defaultHandler
+  respond response
+
+getFeedHandler :: Stm.TVar Database -> IO Wai.Response
+getFeedHandler database = do
+  db <- Stm.readTVarIO database
+  now <- Time.getCurrentTime
+  let items = getRecentDatabaseItems db now
+  let entries = map itemToEntry items
+  let feed = xmlElement
+        "feed"
+        [("xmlns", "http://www.w3.org/2005/Atom")]
+        ( xmlNode "title" [] [xmlContent "Haskell Weekly"]
+        : xmlNode "id" [] [xmlContent "https://haskellweekly.news/"]
+        : xmlNode "updated" [] [xmlContent (rfc3339 now)]
+        : xmlNode "link" [("rel", "self"), ("href", "http://localhost:3000/feed.atom")] []
+        : entries
+        )
+  let document = xmlDocument feed
+  pure (Wai.responseLBS
+    Http.ok200
+    [(Http.hContentType, Text.encodeUtf8 (Text.pack "application/atom+xml"))]
+    (Xml.renderLBS Xml.def document))
+
+getHealthCheckHandler :: Applicative m => m Wai.Response
+getHealthCheckHandler = pure (Wai.responseLBS Http.ok200 [] LazyBytes.empty)
+
+getRobotsHandler :: Applicative m => m Wai.Response
+getRobotsHandler = pure (Wai.responseLBS
+  Http.ok200
+  []
+  (LazyBytes.fromStrict (Text.encodeUtf8 (Text.pack (unlines
+    [ "User-Agent: *"
+    , "Disallow:"
+    ])))))
+
+getFaviconHandler :: Applicative m => m Wai.Response
+getFaviconHandler = pure (Wai.responseLBS
+  Http.ok200
+  []
+  (LazyBytes.pack
+    [ 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x10, 0x10
+    , 0x02, 0x00, 0x01, 0x00, 0x01, 0x00, 0xb0, 0x00
+    , 0x00, 0x00, 0x16, 0x00, 0x00, 0x00, 0x28, 0x00
+    , 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x20, 0x00
+    , 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00
+    , 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x79, 0x48
+    , 0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    , 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    ]))
+
+getIndexHandler :: Stm.TVar Database -> IO Wai.Response
+getIndexHandler database = do
+  db <- Stm.readTVarIO database
+  now <- Time.getCurrentTime
+  let items = getRecentDatabaseItems db now
+  let
+    settings = Xml.def { Xml.rsXMLDeclaration = False }
+    doctype = Xml.Doctype (Text.pack "html") Nothing
+    prologue = Xml.Prologue [] (Just doctype) []
+    htmlHead = xmlNode "head" []
+      [ xmlNode "meta" [("charset", "utf-8")] []
+      , xmlNode "title" [] [xmlContent "Haskell Weekly"]
+      , xmlNode "link"
+        [ ("rel", "alternate")
+        , ("type", "application/atom+xml")
+        , ("href", "feed.atom")
+        ] []
+      ]
+    htmlBody = xmlNode "body" []
+      [ xmlNode "h1" [] [xmlContent "Haskell Weekly"]
+      , xmlNode "p" []
+        [ xmlContent "Subscribe to "
+        , xmlNode "a" [("href", "feed.atom")] [xmlContent "the Atom feed"]
+        , xmlContent "."
+        ]
+      , xmlNode "ol" [] (map
+        (\ (author, item) -> xmlNode "li" []
+          [ xmlNode "a" [("href", fromUrl (itemUrl item))] [xmlContent (fromName (itemName item))]
+          , xmlContent " by "
+          , xmlContent (fromName (authorName author))
+          , xmlContent " on "
+          , xmlContent (Time.formatTime Time.defaultTimeLocale "%B %-e" (itemTime item))
+          ])
+        items)
+      ]
+    html = xmlElement "html" [] [htmlHead, htmlBody]
+    document = Xml.Document prologue html []
+    body = Xml.renderLBS settings document
+  pure (Wai.responseLBS
+    Http.ok200
+    [(Http.hContentType, Text.encodeUtf8 (Text.pack "text/html; charset=utf-8"))]
+    body)
+
+defaultHandler :: Applicative m => m Wai.Response
+defaultHandler = pure (Wai.responseLBS Http.notFound404 [] LazyBytes.empty)
 
 xmlName :: String -> Xml.Name
 xmlName string = Xml.Name (Text.pack string) Nothing Nothing
