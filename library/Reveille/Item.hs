3d01ee919e79b4ff0a7ba1435400e99050ce458c
Taylor Fausak <taylor@fausak.me>
2018-02-17 09:00:25 -0500

    Return a structured error from item creation

--- library/Reveille/Item.hs
+++ library/Reveille/Item.hs
@@ -1,11 +1,13 @@
 module Reveille.Item
   ( Item(..)
   , toItem
+  , ItemError(..)
   ) where
 
-import Reveille.Name (Name, toName)
-import Reveille.Url (Url, toUrl)
+import Reveille.Name (Name, toName, NameError)
+import Reveille.Url (Url, toUrl, UrlError)
 
+import qualified Control.Arrow as Arrow
 import qualified Data.Text as Text
 import qualified Data.Time as Time
 import qualified Text.Feed.Query as Feed
@@ -17,14 +19,14 @@ data Item = Item
   , itemTime :: Time.UTCTime
   } deriving (Eq, Ord, Show)
 
-toItem :: Feed.Item -> Either String Item
+toItem :: Feed.Item -> Either ItemError Item
 toItem feedItem = do
-  rawName <- maybeToEither "missing item name" (Feed.getItemTitle feedItem)
-  name <- either (Left . show) Right (toName (Text.unpack rawName))
-  rawUrl <- maybeToEither "missing item url" (Feed.getItemLink feedItem)
-  url <- either (Left . show) Right (toUrl (Text.unpack rawUrl))
-  maybeTime <- maybeToEither "missing item time" (Feed.getItemPublishDate feedItem)
-  time <- maybeToEither "invalid item time" maybeTime
+  rawName <- maybeToEither ItemErrorNoName (Feed.getItemTitle feedItem)
+  name <- Arrow.left ItemErrorBadName (toName (Text.unpack rawName))
+  rawUrl <- maybeToEither ItemErrorNoUrl (Feed.getItemLink feedItem)
+  url <- Arrow.left ItemErrorBadUrl (toUrl (Text.unpack rawUrl))
+  maybeTime <- maybeToEither ItemErrorNoTime (Feed.getItemPublishDate feedItem)
+  time <- maybeToEither ItemErrorInvalidTime maybeTime
   pure Item
     { itemName = name
     , itemUrl = url
@@ -35,3 +37,12 @@ maybeToEither :: l -> Maybe r -> Either l r
 maybeToEither l m = case m of
   Nothing -> Left l
   Just r -> Right r
+
+data ItemError
+  = ItemErrorNoName
+  | ItemErrorBadName NameError
+  | ItemErrorNoUrl
+  | ItemErrorBadUrl UrlError
+  | ItemErrorNoTime
+  | ItemErrorInvalidTime
+  deriving (Eq, Ord, Show)
